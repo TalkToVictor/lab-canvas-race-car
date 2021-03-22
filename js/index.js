fb72bc735f7b4b40a47b22b9a3e3bcbb1a17d3a2
TalkToVictor <itsvictornwani@yahoo.com>
2021-03-22 19:04:22 -0400

    pushat

--- js/index.js
+++ js/index.js
@@ -27,12 +27,3 @@ window.onload = () => {
 
   function endGame() { }
 };
-window.onload = () => {
-  document.getElementById('start-button' || 'end-button').onclick = () => {
-    startGame();
-  };
-
-  function startGame() { }
-
-  function endGame() { }
-};
