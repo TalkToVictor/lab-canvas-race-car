76ed9a6ca7ce4945f81aaba0d45e791210fcf7da
TalkToVictor <itsvictornwani@yahoo.com>
2021-03-22 00:45:12 -0400

    updated

--- js/index.js
+++ js/index.js
@@ -9,3 +9,30 @@ window.onload = () => {
 };
 
 
+window.onload = () => {
+  document.getElementById('start-button' || 'end-button').onclick = () => {
+    startGame();
+  };
+
+  function startGame() { }
+
+  function endGame() { }
+};
+window.onload = () => {
+  document.getElementById('start-button' || 'end-button').onclick = () => {
+    startGame();
+  };
+
+  function startGame() { }
+
+  function endGame() { }
+};
+window.onload = () => {
+  document.getElementById('start-button' || 'end-button').onclick = () => {
+    startGame();
+  };
+
+  function startGame() { }
+
+  function endGame() { }
+};
